d5ae4a1dcdcdc9b71eeef776bc1eb1cdf5e72349
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-04-20 06:30:47 +0900

    [Add]マウスカーソルイベント,class

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 // マウスオーバー
 $(function(){
   $('.box1').mouseover(function(){
-    $('.box1').css({'background-color':'#0000FF'});
+    $('.box1').addClass('box1-ext');
   });
-});
-
-// マウスアウト
-$(function(){
   $('.box1').mouseout(function(){
-    $('.box1').css({'background-color':'#FF0000'});
+    $('.box1').removeClass('box1-ext');
   });
 });
